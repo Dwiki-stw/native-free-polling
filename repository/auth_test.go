3edc4f6b977502dbac49411b5dd16f6b25e34830
setw <dwiki.stw12@gmail.com>
2025-10-10 10:39:21 +0700

    test(auth-repo): add unit tests and clarify integration tests

--- repository/auth_test.go
+++ repository/auth_test.go
@@ -2,58 +2,90 @@ package repository
 
 import (
 	"context"
-	"native-free-pollings/database"
+	"database/sql"
+	"native-free-pollings/domain"
 	"native-free-pollings/models"
+	"regexp"
 	"testing"
 	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateUser(t *testing.T) {
-	conf := Get()
-	db := database.GetDatabaseConnection(conf.Database)
-	defer db.Close()
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	user := &models.User{
-		Email:        "dwiki@gmail.com",
-		PasswordHash: "hash123",
-		Name:         "dwiki",
+func setupAuthMockDB(t *testing.T) (*sql.DB, sqlmock.Sqlmock, domain.AuthRepository) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("failed to open sqlmock: %v", err)
 	}
 
 	repo := NewAuth(db)
 
-	err := repo.CreateUser(ctx, user)
-	if err != nil {
-		t.Fatal("Failed create user:", err)
-	}
+	return db, mock, repo
 }
 
-func TestGetUserByEmail(t *testing.T) {
-	conf := Get()
-	db := database.GetDatabaseConnection(conf.Database)
+func TestAuthRepository_CreateUser(t *testing.T) {
+	db, mock, repo := setupAuthMockDB(t)
 	defer db.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	expected := &models.User{
-		Email:        "dwiki@gmail.com",
-		PasswordHash: "hash123",
-		Name:         "dwiki",
+	user := &models.User{
+		Email:        "test@example.com",
+		PasswordHash: "test123",
+		Name:         "test user",
 	}
 
-	repo := NewAuth(db)
-	user, err := repo.GetUserByEmail(ctx, "dwiki@gmail.com")
-	if err != nil {
-		t.Fatal("user not found:", err)
+	expectedId := int64(1)
+	expectedCreatedAt := time.Now()
+	expectedUpdatedAt := time.Now()
+
+	rows := sqlmock.NewRows([]string{"id", "created_at", "updated_at"}).
+		AddRow(expectedId, expectedCreatedAt, expectedUpdatedAt)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`
+		INSERT INTO users (email, password_hash, name)
+        VALUES ($1, $2, $3)
+        RETURNING id, created_at, updated_at
+	`)).
+		WithArgs(user.Email, user.PasswordHash, user.Name).
+		WillReturnRows(rows)
+
+	err := repo.CreateUser(context.Background(), user)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedId, user.ID)
+	assert.Equal(t, expectedCreatedAt, user.CreatedAt)
+	assert.Equal(t, expectedUpdatedAt, user.UpdatedAt)
+}
+
+func TestAuthRepository_GetByEmail(t *testing.T) {
+	db, mock, repo := setupAuthMockDB(t)
+	defer db.Close()
+
+	expedtedUser := &models.User{
+		ID:           1,
+		Email:        "test@example.com",
+		PasswordHash: "test123",
+		Name:         "test user",
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
 	}
 
-	assert.NotNil(t, user)
-	assert.Equal(t, expected.Email, user.Email)
-	assert.Equal(t, expected.PasswordHash, user.PasswordHash)
-	assert.Equal(t, expected.Name, user.Name)
+	rows := sqlmock.NewRows([]string{"id", "email", "password_hash", "name", "created_at", "updated_at"}).
+		AddRow(expedtedUser.ID, expedtedUser.Email, expedtedUser.PasswordHash, expedtedUser.Name, expedtedUser.CreatedAt, expedtedUser.UpdatedAt)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`
+		SELECT id, email, password_hash, name, created_at, updated_at
+        FROM users
+        WHERE email = $1
+		LIMIT 1 
+	`)).
+		WithArgs(expedtedUser.Email).
+		WillReturnRows(rows)
+
+	user, err := repo.GetUserByEmail(context.Background(), expedtedUser.Email)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expedtedUser.ID, user.ID)
+	assert.Equal(t, expedtedUser.Email, user.Email)
+	assert.Equal(t, expedtedUser.Name, user.Name)
 }
